877dc6e97c8aa4a4d001bec4fa7047b8daeda183
yingzhang0 <ying_zhang0000@163.com>
2022-04-20 09:22:01 +0800

    fixed bugs on bayes_corr

--- GeneClust/steps/similarity.py
+++ GeneClust/steps/similarity.py
@@ -31,7 +31,7 @@ def compute_gene_similarity(
     if metric in ('pearson', 'spearman', 'kendall'):
         adata.varp[metric] = adata.varm[dr_method].T.corr(metric).abs()  # the cols represent genes after transpose
     elif metric == 'bayes_corr':
-        adata.varp[metric] = bayes_corr(adata.varm[dr_method])
+        adata.varp[metric] = bayes_corr(pd.DataFrame(adata.raw.X.T, index = adata.raw.var_names))
     elif metric == 'mutual_info':
         adata.varp[metric] = mutual_info(adata.varm[dr_method])
     elif metric == 'euclidean_dis':
@@ -45,7 +45,7 @@ def compute_gene_similarity(
 def bayes_corr(data: pd.DataFrame):
     """
     similarity measure using Bayesian correlation 
-    :param data: rows: genes 
+    :param data: raw data (row:gene) 
     """
     nrowsX = data.shape[0]
     ncolsX = data.shape[1]
@@ -57,33 +57,43 @@ def bayes_corr(data: pd.DataFrame):
     alphasPLUSbetas = alphas + betas
     alp_alpPLUSbeta = alphas / alphasPLUSbetas
     Psi = alp_alpPLUSbeta - np.array([x / ncolsX for x in alp_alpPLUSbeta.sum(axis=1)] * ncolsX).reshape(-1, nrowsX).T
-    var_vec = ((((alphas * betas) / ((alphasPLUSbetas ** 2) * (alphasPLUSbetas + 1))).sum(axis=1) + (Psi ** 2).sum(
-        axis=1)) / ncolsX).reshape(nrowsX, 1)
+    var_vec = ((((alphas * betas) / ((alphasPLUSbetas ** 2) * (alphasPLUSbetas + 1))).sum(axis=1) 
+                + (Psi ** 2).sum(axis=1)) / ncolsX).reshape(nrowsX, 1)
     cov_mtrx = np.dot(Psi, Psi.T) / ncolsX
-    Bcorrvals = cov_mtrx / np.sqrt(np.dot(var_vec, var_vec.T))
+    Bcorrvals = cov_mtrx / np.sqrt(np.dot(var_vec, var_vec.T)) 
     Bcorrvals[np.diag_indices_from(Bcorrvals)] = 1
 
     return pd.DataFrame(Bcorrvals, columns=data.index, index=data.index)
 
-
 def mutual_info(data: pd.DataFrame):
     """
     similarity measure using mutual information
-    :param data: rows: genes 
+    :param data: row: gene
     """
-    # empty similarity matrix. Column i is the mutual information between the i_th gene and all genes
-    simi_matrix = pd.DataFrame(columns=data.index, index=data.index)
+    #calculate mutual_info between different genes(pairwise)
+    simi_matrix = squareform(pdist(data, lambda u, v: mutual_info_regression(np.array(u).reshape(-1, 1), v, random_state = 40)))
+    simi_matrix = pd.DataFrame(simi_matrix, columns = data.index, index = data.index)
+    #calculate mutual_info between a gene and itself
+    s = [float(mutual_info_regression(np.array(data[i]).reshape(-1,1),data[i])) for i in range(len(data))]
+    simi_matrix = simi_matrix + np.diag(s)
+
+    return simi_matrix
     # In each iteration, calculate the mi between i_th gene and all genes
     #data_array shape(ngene, ncell)
-    data_array = data.values
-    for i in range(len(data)):
-        simi_matrix[i] = mutual_info_regression(data_array.T, data.iloc[i,], random_state = 40)
-    return simi_matrix
+    #data_array = data.values
+    # for i in range(len(data)):
+        # simi_matrx[i] = mutual_info_regression(data_array.T, data.iloc[i,], random_state = 40)
+        #Or try this 
+        # simi_matrix.iloc[i:, i] = mutual_info_regression(data_array[i:].T, data.iloc[i,], random_state = 40)
+        # t = simi_matrix.T.copy()
+        # np.fill_diagonal(t.values, 0)
+        # simi_matrix = simi_matrix + t
+
 
 def euclidean_dis(data: pd.DataFrame):
     """
     similarity measure using euclidean distance
-    :param data: rows: genes
+    :param data: row: gene
     """
     dist = pd.DataFrame(squareform(pdist(data, 'euclidean')), columns = data.index, index = data.index)
     return dist
