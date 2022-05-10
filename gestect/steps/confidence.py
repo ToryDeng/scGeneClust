e9a448e2ea306ffedfa71c8112affbcb72c7cc66
yingzhang0 <ying_zhang0000@163.com>
2022-05-10 20:25:42 +0800

    added 'mnn_proba' to find_confident_cells

--- gestect/steps/confidence.py
+++ gestect/steps/confidence.py
@@ -7,7 +7,9 @@ import numpy as np
 from queue import Queue
 from typing import Literal
 from sklearn.mixture import GaussianMixture
-
+from sympy import Q
+from sklearn.neighbors import NearestNeighbors
+import pandas as pd
 
 def find_confident_cells(expr: np.ndarray,
                          n_cell_clusters: int,
@@ -23,6 +25,30 @@ def find_confident_cells(expr: np.ndarray,
         max_proba = cell_proba.max(1)
         is_confident = max_proba > 0.95
         cluster_label = cell_proba.argmax(1)
+    if how == 'mnn_proba':
+        #find confident cells using gmm
+        gmm = GaussianMixture(n_components=n_cell_clusters)
+        gmm.fit(expr)
+        cell_proba = gmm.predict_proba(expr)
+        max_proba = cell_proba.max(1)
+        gmm_is_confident = max_proba > 0.95
+        cluster_label = cell_proba.argmax(1)
+        confident_cluster_label = cluster_label[gmm_is_confident]  #cluster label of confident cells
+        gmm_conf_expr = expr[gmm_is_confident]                     #expr of confident cells after gmm
+
+        #find mnn in each cluster
+        is_confident = pd.DataFrame(columns=['is_conf'], index=gmm_conf_expr.index)
+        is_confident['is_conf'] = False
+        for i in range(0, n_cell_clusters):                        #for each cluster
+            cluster_i = cluster_label == i
+            nbrs = NearestNeighbors(n_neighbors=10).fit(gmm_conf_expr[cluster_i])
+            indices = nbrs.kneighbors(gmm_conf_expr, return_distance = False)
+            for j in range(len(indices)):           
+                for neighbor in indices[j]:                        #find all neighbor of cell j
+                    if j in indices[neighbor]:                     #if cell j is also the neighbor of its neighbor
+                        is_confident['is_conf'][list(gmm_conf_expr[cluster_i].index)[j]] = True
+                        is_confident['is_conf'][list(gmm_conf_expr[cluster_i].index)[neighbor]] = True
+        is_confident = np.array(is_confident['is_conf'])
     else:
         raise NotImplementedError(f"{how} has not been implemented!")
     queue.put({'cluster': cluster_label, 'confident': is_confident})
